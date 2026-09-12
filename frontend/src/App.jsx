b15893994288b576f698c567ed2ff2e9d6955585
Nisal <91654417+nisalindusara@users.noreply.github.com>
2026-09-13 01:18:13 +0530

    backend serves frontend - frontend modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 
 import { BlockMath, InlineMath } from "react-katex";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_BASE || "";
 
 const FONTFAMILY = "'Google Sans', sans-serif";
 
